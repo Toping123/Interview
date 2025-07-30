8f2fe86ada24bfb3d753f2197b784dfa571247aa
luotaoping <luotaoping@pudu.com>
2025-07-30 19:02:27 +0800

    添加协程工程

---
+++ Coroutine/src/ActiveTest.kt
@@ -0,0 +1,47 @@
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class ActiveTest {
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            runBlocking {
+                val activeTest = ActiveTest()
+                repeat(100) {
+                    activeTest.startActive()
+                }
+                activeTest.cancelActive()
+
+                delay(5_000)
+            }
+        }
+    }
+
+    private var activeJob: Job? = null
+    private var needExecute = true
+
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+    private fun startActive() {
+        if (needExecute || activeJob?.isActive == true) {
+            needExecute = false
+            activeJob?.cancel()
+            activeJob = scope.launch {
+                println("1111111111")
+                delay(1000)
+                println("222222222222")
+            }
+        }
+    }
+
+    private fun cancelActive() {
+        println("cancelActive")
+        activeJob?.cancel()
+    }
+}
